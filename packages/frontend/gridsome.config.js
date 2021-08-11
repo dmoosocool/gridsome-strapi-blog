84a0a2339bdd7fc72c0567cc361106aa3114feef
DM <dmoosocool@gmail.com>
2021-08-11 20:54:34 +0800

    feat: use product ip.

--- packages/frontend/gridsome.config.js
+++ packages/frontend/gridsome.config.js
@@ -10,7 +10,7 @@ module.exports = {
     {
       use: "@gridsome/source-strapi",
       options: {
-        apiURL: "http://localhost:1337",
+        apiURL: "http://42.240.136.102:1337",
         queryLimit: 1000,
         contentTypes: ["category", "project", "journal"]
       }
