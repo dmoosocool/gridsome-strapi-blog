05fd331c7076e0c9c4be885a2873ba5c953f38f7
DM <dmoosocool@gmail.com>
2021-08-12 14:07:39 +0800

    feat: support http-server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,11 +2,12 @@ module.exports = {
   apps: [
     {
       name: "backend",
-      script: "cd packages/backend && npm run develop"
+      script: "cd packages/backend && npm run build && npm start"
     },
     {
       name: "frontend",
-      script: "cd packages/frontend && npm run develop"
+      script:
+        "cd packages/frontend && npm run build && npx http-server ./dist -a"
     }
   ]
 };
